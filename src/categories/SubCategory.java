9a2f09f48b77b8f3140e84c6465f751f3c9da737
Jeffrey O'Connell <jboconne@bu.edu>
2021-01-26 23:30:54 -0500

    Remove old commented code

--- src/categories/SubCategory.java
+++ src/categories/SubCategory.java
@@ -27,33 +27,3 @@ public class SubCategory extends Category{
 	}
 
 }
-
-/*
-public class NonHeadingFragment extends Category {
-	
-	public NonHeadingFragment() {
-		super();
-	}
-	
-	public NonHeadingFragment(String aString) {
-		super(aString);
-	}
-
-	public void display() {
-		System.out.println("\t" + fragmentText);			
-	}	
-
-	public void setText(String someText) {
-		fragmentText = someText.substring(0, someText.length());
-	}
-	
-	public String getText() {
-		return fragmentText;
-	}
-	
-	public String getType() {
-		return "Non-heading fragment";
-	}
-	
-}
-*/
